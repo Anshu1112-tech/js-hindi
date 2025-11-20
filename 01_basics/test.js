0ae562b1f1cad252073c963a78c5e66c04636968
Anshu230806 <ay0354760@gmail.com>
2025-11-20 17:19:50 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
 console.log("anshu")
+const accountId = 355
+let accountPassword="1234"
+var accountMail = "adh@gmail.com"
+accountAddress = "jaipur"
+let accountState
+// donot use var due to scope issue it is global 
+// accountId = 2 not allowed 
+// use semicolon or not no issue 
+accountPassword="123"
+var accountMail = "h@gmail.com"
+accountAddress = "rampur"
+console.table([accountId , accountPassword,accountMail , accountAddress , accountState ])
