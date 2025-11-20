31f313bda139e68134c2738267fbc9f2910e8d56
Anshu230806 <ay0354760@gmail.com>
2025-11-20 18:05:25 +0000

    operatins

--- 01_basics/03_datatypeconversion.js
+++ 01_basics/03_datatypeconversion.js
@@ -51,4 +51,43 @@ let stringnumber = String(somenumber)
 console.log(stringnumber);  // 70 
 
 
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num8, num9
+
+num1 = num8 = num9 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
